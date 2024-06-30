c38fd93870a2798c3daa5126446b5db3ef717a02
marumur <164278710+marumur@users.noreply.github.com>
2024-06-30 21:20:22 +0800

    Update test_calc.py

--- test_calc.py
+++ test_calc.py
@@ -7,7 +7,7 @@ class TestCalc:
         assert self.calc.adding(self, 1, 1) == 2
     def test_adding_unsuccess(self):
         assert self.calc.adding(self, 1, 1) == 3
-    def test_zero_divdsion(self):
+    def test_zero_division(self):
         with pytest.raises(ZeroDivisionError):
             self.calc.division(self, 1, 0)
 
